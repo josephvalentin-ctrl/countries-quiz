dd2118781da639edfbf2603df68b2a27d30f18ac
Joseph Valentin <joseph.valentin@mediconesystems.com>
2026-07-16 10:55:03 +0000

    file setup

---
+++ app/Http/Controllers/quiz_controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class quiz_controller extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+    public function post_answer()
+    {
+
+    }
+
+}
+
